89718715ac344c12630e76eb98f5f61f8874ddd6
David Wood <david@davidtw.co>
2017-02-21 09:26:09 +0000

    Added tests back to this repo after accidently mv'ing them instead of copying.

---
+++ tests/tests_chapter9.py
@@ -0,0 +1,176 @@
+from __future__ import print_function
+# Chapter 3
+from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.core.urlresolvers import reverse
+import os
+
+#Chapter 4
+from django.contrib.staticfiles import finders
+
+#Chapter 5
+from rango.models import Page, Category
+import populate_rango
+from . import test_utils
+
+#Chapter 6
+from .decorators import chapter6
+
+#Chapter 7
+from .decorators import chapter7
+from rango.forms import CategoryForm, PageForm
+
+#Chapter 8
+from django.template import loader
+from django.conf import settings
+from .decorators import chapter8
+import os.path
+
+#Chapter 9
+from rango.models import User, UserProfile
+from rango.forms import UserForm, UserProfileForm
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.files.storage import default_storage
+from .decorators import chapter9
+
+# ===== Chapter 9
+class Chapter9ModelTests(TestCase):
+    def test_user_profile_model(self):
+        # Create a user
+        user, user_profile = test_utils.create_user()
+
+        # Check there is only the saved user and its profile in the database
+        all_users = User.objects.all()
+        self.assertEquals(len(all_users), 1)
+
+        all_profiles = UserProfile.objects.all()
+        self.assertEquals(len(all_profiles), 1)
+
+        # Check profile fields were saved correctly
+        all_profiles[0].user = user
+        all_profiles[0].website = user_profile.website
+
+class Chapter9ViewTests(TestCase):
+
+    @chapter9
+    def test_registration_form_is_displayed_correctly(self):
+        #Access registration page
+        try:
+            response = self.client.get(reverse('register'))
+        except:
+            try:
+                response = self.client.get(reverse('rango:register'))
+            except:
+                return False
+
+        # Check if form is rendered correctly
+        # self.assertIn('<h1>Register with Rango</h1>', response.content)
+        self.assertIn(b'<strong>register here!</strong><br />'.lower(), response.content.lower())
+
+        # Check form in response context is instance of UserForm
+        self.assertTrue(isinstance(response.context['user_form'], UserForm))
+
+        # Check form in response context is instance of UserProfileForm
+        self.assertTrue(isinstance(response.context['profile_form'], UserProfileForm))
+
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+        # Check form is displayed correctly
+        self.assertEquals(response.context['user_form'].as_p(), user_form.as_p())
+        self.assertEquals(response.context['profile_form'].as_p(), profile_form.as_p())
+
+        # Check submit button
+        self.assertIn(b'type="submit" name="submit" value="Register"', response.content)
+
+    @chapter9
+    def test_login_form_is_displayed_correctly(self):
+        #Access login page
+        try:
+            response = self.client.get(reverse('login'))
+        except:
+            try:
+                response = self.client.get(reverse('rango:login'))
+            except:
+                return False
+
+        #Check form display
+        #Header
+        self.assertIn(b'<h1>Login to Rango</h1>'.lower(), response.content.lower())
+
+        #Username label and input text
+        self.assertIn(b'Username:', response.content)
+        self.assertIn(b'input id="id_username" maxlength="50" name="username" type="text" required', response.content)
+
+        #Password label and input text
+        self.assertIn(b'Password:', response.content)
+        self.assertIn(b'input id="id_password" name="password" type="password" required', response.content)
+
+        #Submit button
+        self.assertIn(b'input type="submit" value="submit"', response.content)
+
+    @chapter9
+    def test_login_provides_error_message(self):
+        # Access login page
+        try:
+            response = self.client.post(reverse('login'), {'username': 'wronguser', 'password': 'wrongpass'})
+        except:
+            try:
+                response = self.client.post(reverse('rango:login'), {'username': 'wronguser', 'password': 'wrongpass'})
+            except:
+                return False
+
+        print(response.content)
+        try:
+            self.assertIn(b'wronguser', response.content)
+        except:
+            self.assertIn(b'Invalid login details supplied.', response.content)
+
+    @chapter9
+    def test_login_redirects_to_index(self):
+        # Create a user
+        test_utils.create_user()
+
+        # Access login page via POST with user data
+        try:
+            response = self.client.post(reverse('login'), {'username': 'testuser', 'password': 'test1234'})
+        except:
+            try:
+                response = self.client.post(reverse('rango:login'), {'username': 'testuser', 'password': 'test1234'})
+            except:
+                return False
+
+        # Check it redirects to index
+        self.assertRedirects(response, reverse('index'))
+
+    @chapter9
+    def test_upload_image(self):
+        # Create fake user and image to upload to register user
+        image = SimpleUploadedFile("testuser.jpg", "file_content", content_type="image/jpeg")
+        try:
+            response = self.client.post(reverse('register'),
+                             {'username': 'testuser', 'password':'test1234',
+                              'email':'testuser@testuser.com',
+                              'website':'http://www.testuser.com',
+                              'picture':image } )
+        except:
+            try:
+                response = self.client.post(reverse('rango:register'),
+                                 {'username': 'testuser', 'password':'test1234',
+                                  'email':'testuser@testuser.com',
+                                  'website':'http://www.testuser.com',
+                                  'picture':image } )
+            except:
+                return False
+
+        # Check user was successfully registered
+        self.assertIn(b'thank you for registering!'.lower(), response.content.lower())
+        user = User.objects.get(username='testuser')
+        user_profile = UserProfile.objects.get(user=user)
+        path_to_image = './media/profile_images/testuser.jpg'
+
+        # Check file was saved properly
+        self.assertTrue(os.path.isfile(path_to_image))
+
+        # Delete fake file created
+        default_storage.delete('./media/profile_images/testuser.jpg')
